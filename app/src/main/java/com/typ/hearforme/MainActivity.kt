62ce70b24e7d3fe80eb5790547e106ba383e68a1
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2025-12-19 18:36:29 +0200

    feat(app): Introduce navigation and viewmodel
    This commit introduces `MainNavigation` to handle the app's navigational structure and integrates the `DashboardViewModel`.
    
    It also sets up the necessary dependency injections for `AlertManager` and `DashboardViewModel` using Koin. The `MainActivity` is updated to use the new `MainNavigation` composable.

--- app/src/main/java/com/typ/hearforme/MainActivity.kt
+++ app/src/main/java/com/typ/hearforme/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.typ.hearforme
 
+import MainNavigation
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import com.typ.hearforme.designsystem.theme.HearForMeTheme
 import com.typ.hearforme.service.SoundDetectionService
-import com.typ.hearforme.ui.theme.HearForMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,20 +16,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             HearForMeTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    contentColor = MaterialTheme.colorScheme.primary,
-                    containerColor = MaterialTheme.colorScheme.primaryContainer
-                ) { innerPadding ->
-                }
+                MainNavigation()
             }
         }
 
-        startService(
-            Intent(
-                this,
-                SoundDetectionService::class.java
-            )
-        )
+        startService(Intent(this, SoundDetectionService::class.java))
     }
 }
