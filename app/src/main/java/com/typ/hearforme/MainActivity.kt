e4cb5f31bc4662e45ffa2a635bd4947dfaf59c8f
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2025-12-19 14:11:08 +0200

    repo: initial commit.

---
+++ app/src/main/java/com/typ/hearforme/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.typ.hearforme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.typ.hearforme.ui.theme.HearForMeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            HearForMeTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ) { innerPadding ->
+                }
+            }
+        }
+    }
+}
