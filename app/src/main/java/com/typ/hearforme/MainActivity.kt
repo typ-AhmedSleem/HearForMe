4990c72e7d91c3df1c2e1ddd074a9cd503f8ee92
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2025-12-19 17:44:36 +0200

    feat(service): Start SoundDetectionService from MainActivity
    This commit introduces the automatic startup of the `SoundDetectionService` when the `MainActivity` is created. An intent is now used to start the service, ensuring it runs in the background.

--- app/src/main/java/com/typ/hearforme/MainActivity.kt
+++ app/src/main/java/com/typ/hearforme/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.typ.hearforme
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.typ.hearforme.service.SoundDetectionService
 import com.typ.hearforme.ui.theme.HearForMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,5 +27,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        startService(
+            Intent(
+                this,
+                SoundDetectionService::class.java
+            )
+        )
     }
 }
