5427ffbae642828d7fb590a5caa2abf126ec8e9d
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2025-12-23 11:45:08 +0200

    feat(data): Integrate SQLDelight for database operations
    This commit introduces the SQLDelight library to the project for managing the local database.
    
    It includes the following changes:
    - Adds the SQLDelight plugin to the project's root `build.gradle.kts` and `gradle/libs.versions.toml`.
    - Applies the plugin to the `data` module.
    - Adds `sqldelight-android-driver` and `sqldelight-coroutines-extensions` as dependencies.
    - Configures a `HistoryDatabase` within the `data` module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.sqldelight) apply false
 }
