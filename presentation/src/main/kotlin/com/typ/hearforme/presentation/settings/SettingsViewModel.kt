1a0ec5b476a92c5193befab8802cf4e791dda423
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-02-12 08:34:24 +0200

    ref(settings): Mark SoundSetting as Immutable
    This commit adds the `@Immutable` annotation to the `SoundSetting` data class.
    
    By explicitly marking the class as immutable, we provide a signal to the Compose compiler that it can safely skip recomposition if the object's instance has not changed, leading to better performance and more efficient UI updates.

--- presentation/src/main/kotlin/com/typ/hearforme/presentation/settings/SettingsViewModel.kt
+++ presentation/src/main/kotlin/com/typ/hearforme/presentation/settings/SettingsViewModel.kt
@@ -1,5 +1,6 @@
 package com.typ.hearforme.presentation.settings
 
+import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.typ.hearforme.domain.model.SoundType
@@ -12,6 +13,7 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+@Immutable
 data class SoundSetting(
     val type: SoundType,
     val isEnabled: Boolean,
