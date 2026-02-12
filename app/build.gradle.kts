f355074b0ab9b23492b92634e17deffc66fc09f5
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-02-12 06:50:24 +0200

    gradle: pumped app version to "1.1" and versionCode to '2'.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.typ.hearforme"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
