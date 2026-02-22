46c50ca21bfd5ffefec7873970154a086375492f
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-02-22 16:56:16 +0200

    build(app): Update SDK and version information
    This commit updates the `minSdk` version and increments the app's version code and name.
    
    The `minSdk` has been raised from 29 to 32. The `versionCode` is updated from 2 to 3, and the `versionName` is changed from "1.1" to "1.2".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
 
     defaultConfig {
         applicationId = "com.typ.hearforme"
-        minSdk = 29
+        minSdk = 32
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
